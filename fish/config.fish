5553afdc6afe3a99a49801ebc42fd28081e846b0
safin-sys <mdsafinofficial@gmail.com>
2025-12-31 00:59:11 +0600

    new hyprland v3 conf changes

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set -gx TERM xterm-256color
 starship init fish | source
 
 if test -z "$DISPLAY" -a (tty) = "/dev/tty1"
-    exec Hyprland >/dev/null 2>&1
+    exec start-hyprland >/dev/null 2>&1
 end
 
 # pnpm
